6c414d349b8656b6a74e2dbc77b8cedb9019c0e7
Matthias Endler <matthias.endler@trivago.com>
2015-01-26 23:23:51 +0100

    Add simple usage example

--- test/mre/ProcessTest.php
+++ test/mre/ProcessTest.php
@@ -5,15 +5,20 @@ class ProcessTest extends PHPUnit_Framework_TestCase
     /** @var  \mre\Process */
     private $oProcess;
 
-    const COMMAND = 'cat';
-
     protected function setUp()
     {
-        $this->oProcess = new \mre\Process(self::COMMAND);
     }
 
     public function testCorrectCommand()
     {
-        $this->assertEquals(self::COMMAND, $this->oProcess->getCommand());
+        $this->oProcess = new \mre\Process('cat');
+        $this->assertEquals('cat', $this->oProcess->getCommand());
     }
+
+    public function testIncorrectCommand()
+    {
+        $this->oProcess = new \mre\Process('PresumablyThereIsNoSuchCommandInYourPATH');
+        $this->oProcess->send('tewst');
+    }
+
 }
